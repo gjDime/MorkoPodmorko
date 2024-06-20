6c770c030d41c8d15341f5f9ec8142620ac323ae
gjDime <dime.gorgiev.03@gmail.com>
2024-06-20 22:53:12 +0200

    Fish scenes added, blackjack assets

---
+++ scripts/Main/Area2DBlueFish.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public partial class Area2DBlueFish : Area2D
+{
+	private bool entered = false;
+	// Called when the node enters the scene tree for the first time.
+
+	private void BodyEntered(Node2D node)
+	{
+		entered = true;
+		// GD.Print(entered);
+	}
+	
+	
+	private void BodyExcited(Node2D node)
+	{
+		entered = false;
+		// GD.Print(entered);
+	}
+	
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (entered)
+		{
+			if (Input.IsActionPressed("Interact"))
+			{
+				GetTree().ChangeSceneToFile("res://scenes/Pong_Game.tscn");
+			}
+		}
+	}
+}
---
+++ scripts/Main/Area2DRedFish.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+public partial class Area2DRedFish : Area2D
+{
+	// Called when the node enters the scene tree for the first time.
+	private bool entered = false;
+	// Called when the node enters the scene tree for the first time.
+
+	private void BodyEntered(Node2D node)
+	{
+		entered = true;
+		// GD.Print(entered);
+	}
+	
+	
+	private void BodyExcited(Node2D node)
+	{
+		entered = false;
+		// GD.Print(entered);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (entered)
+		{
+			if (Input.IsActionPressed("Interact"))
+			{
+				GetTree().ChangeSceneToFile("res://scenes/blackjack/blackjack_game.tscn");
+			}
+		}
+	}
+}
