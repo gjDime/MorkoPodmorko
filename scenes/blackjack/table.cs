c9e0fb0f6359a6779f088757fd10d3b310cf4766
gjDime <dime.gorgiev.03@gmail.com>
2024-06-26 18:50:29 +0200

    blackjack finished

--- scenes/blackjack/table.cs
+++ scenes/blackjack/table.cs
@@ -1,16 +1,17 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public partial class table : Node2D
 {
     private List<KeyValuePair<string, int>> deck { get; set; } = new();
 
 
-    private int dealerSum { get; set; }
-    private int playerSum { get; set; }
-    private int playerCards { get; set; }
-    private int dealerCards { get; set; }
+    //private int dealerSum { get; set; }
+    //private int playerSum { get; set; }
+    private List<int> playerCards { get; set; }
+    private List<int> dealerCards { get; set; }
     private Random random = new();
     private static Vector2 PLAYER_CARD_LOCATION = new Vector2(480, 380);
     private static Vector2 DEALER_CARD_LOCATION = new Vector2(480, 100);
@@ -18,18 +19,18 @@ public partial class table : Node2D
     private Button btnReset;
     private Button btnHit;
     private Button btnStay;
+    private Button btnBack;
     private Label lblGameState;
     private Label lblPlayerSum;
     private Label lblDealerSum;
-
+    private int numWins;
     private string cardPath;
 
     private void RefillDeck()
     {
-        playerSum = 0;
-        playerCards = 0;
-        dealerSum = 0;
-        dealerCards = 0;
+        playerCards = new();
+        dealerCards = new();
+
         var types = new[] { "h", "c", "s", "d" };
         for (int i = 1; i <= 13; i++)
             foreach (var type in types)
@@ -39,19 +40,19 @@ public partial class table : Node2D
     private void DealDealer()
     {
         int val = Deal("d");
-        if (val == 1 && dealerSum <= 10)
+        if (val == 1 && dealerCards.Sum() <= 10)
             val = 11;
-        dealerSum += val;
-        dealerCards++;
+
+        dealerCards.Add(val);
     }
 
     private void DealPlayer()
     {
         int val = Deal("p");
-        if (val == 1 && playerSum <= 10)
+        if (val == 1 && playerCards.Sum() <= 10)
             val = 11;
-        playerSum += val;
-        playerCards++;
+
+        playerCards.Add(val);
     }
 
     private int Deal(string flag)
@@ -60,25 +61,20 @@ public partial class table : Node2D
         var value = deck[index].Value;
         var path = deck[index].Key;
         deck.RemoveAt(index);
-
+        
         Card card = ResourceLoader.Load<PackedScene>("res://scenes/blackjack/card.tscn").Instantiate<Card>();
-        Image img = new Image();
-        if (flag == "d" && dealerCards == 1)
+        var sprite = card.GetNode<Sprite2D>("Sprite2D");
+        if (flag == "d" && dealerCards.Count == 1)
         {
             cardPath = path;
-            lblDealerSum.Visible = false;
-            img.Load("res://assets/cards/blank_back.png");
+            sprite.Texture = GD.Load<Texture2D>("res://assets/cards/blank_back.png") as Texture2D;
             card.Name = "Reversed";
         }
         else
-            img.Load(path);
-
-
-        var sprite = card.GetNode<Sprite2D>("Sprite2D");
-        sprite.Texture = ImageTexture.CreateFromImage(img);
+            sprite.Texture = GD.Load<Texture2D>(path);
+        
         Pos(card, flag);
         AddChild(card);
-
         return value;
     }
 
@@ -86,70 +82,75 @@ public partial class table : Node2D
     {
         if (flag == "p")
         {
-            card.GlobalPosition = PLAYER_CARD_LOCATION + new Vector2(playerCards * 20, playerCards * 20);
+            card.GlobalPosition = PLAYER_CARD_LOCATION + new Vector2(playerCards.Count * 20, playerCards.Count * 20);
         }
         else
         {
-            card.GlobalPosition = DEALER_CARD_LOCATION + new Vector2(dealerCards * 20, dealerCards * 20);
+            card.GlobalPosition = DEALER_CARD_LOCATION + new Vector2(dealerCards.Count * 20, dealerCards.Count * 20);
         }
     }
 
     private void HitButtonPressed()
     {
         DealPlayer();
-
-        if (playerSum > 21)
-            GameOver();
-        if (playerSum == 21)
-            BlackJack();
-        
         updateScore();
+
+        if (playerCards.Sum() > 21)
+            GameOver();
+        if (playerCards.Sum() == 21)
+            BlackJack();
     }
 
     private void StayButtonPressed()
     {
         lblDealerSum.Visible = true;
-        Image img = new Image();
         Card card = FindChild("Reversed", false, false) as Card;
         Sprite2D sprite = card.GetNode<Sprite2D>("Sprite2D");
-        img.Load(cardPath);
-        sprite.Texture = ImageTexture.CreateFromImage(img);
+        sprite.Texture = GD.Load<Texture2D>(cardPath);
         //reverse dealers second card
-        
-        
+
         btnHit.Disabled = true;
 
-        while (dealerSum < 17)
+        while (dealerCards.Sum() < 17)
+        {
             DealDealer();
+            updateScore();
+        }
 
-        if (dealerSum > 21)
+        if (dealerCards.Sum() > 21)
             GameWon();
-        else if (dealerSum > playerSum)
+        else if (dealerCards.Sum() > playerCards.Sum())
             GameOver();
-        else if (dealerSum < playerSum)
+        else if (dealerCards.Sum() < playerCards.Sum())
             GameWon();
         else
             GameTie();
-
-
-        updateScore();
     }
 
     private void ResetButtonPressed()
     {
-        GetTree().ReloadCurrentScene();
+        StartNewGame();
+        // GetTree().ReloadCurrentScene();
     }
 
     private void updateScore()
     {
-        lblDealerSum.Text = dealerSum.ToString();
-        lblPlayerSum.Text = playerSum.ToString();
+        if (dealerCards.Contains(11) && dealerCards.Sum() > 21)
+            dealerCards[dealerCards.IndexOf(11)] = 1;
+
+        if (playerCards.Contains(11) && playerCards.Sum() > 21)
+            playerCards[playerCards.IndexOf(11)] = 1;
+
+
+        lblDealerSum.Text = dealerCards.Sum().ToString();
+        lblPlayerSum.Text = playerCards.Sum().ToString();
     }
 
     private void BlackJack()
     {
-        GameEnd();
         lblGameState.Text = "BLACKJACK :) :)";
+        numWins++;
+        GameEnd();
     }
 
     private void GameTie()
@@ -160,10 +161,10 @@ public partial class table : Node2D
 
     private void GameWon()
     {
-        GameEnd();
         lblGameState.Text = "You Won :)";
+        numWins++;
+        GameEnd();
     }
-
 
     private void GameOver()
     {
@@ -176,17 +177,27 @@ public partial class table : Node2D
         btnReset.Visible = true;
         btnHit.Disabled = true;
         btnStay.Disabled = true;
+        if (numWins >= 2)
+            btnBack.Visible = true;
     }
 
-// Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    private void BackButtonPressed()
     {
-        btnReset = GetNode<Button>("ResetButton");
-        btnHit = GetNode<Button>("HitButton");
-        btnStay = GetNode<Button>("StayButton");
-        lblGameState = GetNode<Label>("GameStateLabel");
-        lblPlayerSum = GetNode<Label>("PlayerSum");
-        lblDealerSum = GetNode<Label>("DealerSum");
+        GetTree().ChangeSceneToFile("res://scenes/game.tscn");
+    }
+
+    private void StartNewGame()
+    {
+        foreach (var node in GetChildren().Where(child => child is Card))
+            node.Free();
+        // node.Dispose();
+
+
+        lblDealerSum.Visible = false;
+        lblGameState.Text = "";
+        btnReset.Visible = false;
+        btnHit.Disabled = false;
+        btnStay.Disabled = false;
 
         RefillDeck();
 
@@ -198,12 +209,25 @@ public partial class table : Node2D
 
         updateScore();
 
-        if (playerSum == 21)
+
+        if (playerCards.Sum() == 21)
             BlackJack();
     }
 
-// Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
+    public override void _Ready()
     {
+        btnReset = GetNode<Button>("ResetButton");
+        btnHit = GetNode<Button>("HitButton");
+        btnStay = GetNode<Button>("StayButton");
+        btnBack = GetNode<Button>("GoBackButton");
+        btnBack.Visible = false;
+        lblGameState = GetNode<Label>("GameStateLabel");
+        lblPlayerSum = GetNode<Label>("PlayerSum");
+        lblDealerSum = GetNode<Label>("DealerSum");
+        playerCards = new();
+        dealerCards = new();
+        numWins = 0;
+
+        StartNewGame();
     }
 }
